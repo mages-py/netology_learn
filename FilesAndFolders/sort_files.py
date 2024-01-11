29d0fa30aded9430b89e17310498126968f63527
Evgeniy.Gorbunov <Evgeniy.Gorbunov@polaru.com>
2024-01-11 10:08:49 +0300

    Code review sort_files: change sort method

--- FilesAndFolders/sort_files.py
+++ FilesAndFolders/sort_files.py
@@ -1,29 +1,27 @@
 import os
 
-def get_sorted_file_list(dir_name):
+def get_sorted_file_dict(dir_name):
     files = os.listdir(dir_name)
     sorted_files = {}
     for file in files:
         file_path = os.path.join(dir_name, file)
         if not os.path.isdir(file_path):
-            sorted_files[file] = os.path.getsize(file_path)
-    return dict(sorted(sorted_files.items(), key=lambda item: item[1]))
+            with open(file_path, encoding='utf-8') as f:
+                sorted_files[file] = f.readlines()
+    return dict(sorted(sorted_files.items(), key=lambda item: len(item[1])))
 
 def create_sorted_file(dir_name, sorted_file_name, rewrite_sorted_file = True):
-    if rewrite_sorted_file:
-        open_mode = 'w'
-    else: 
-        open_mode = 'a'
-    
-    list_of_files = list(get_sorted_file_list(dir_name).keys())
+    list_of_files = get_sorted_file_dict(dir_name)
     write_list = []
-    for file in list_of_files:
+    
+    for file, lines in list_of_files.items():
         with open(os.path.join(dir_name, file), encoding='utf-8') as f:
-            lines = f.readlines()
             write_list += [file + '\n', str(len(lines)) + '\n']
             write_list += lines
             write_list += ['\n']
-    with open(sorted_file_name, encoding='utf-8', mode=open_mode) as wf:
+    
+    mode = 'w' if rewrite_sorted_file else 'a'
+    with open(sorted_file_name, encoding='utf-8', mode=mode) as wf:
         wf.writelines(write_list)
         
 
@@ -31,7 +29,7 @@ def create_sorted_file(dir_name, sorted_file_name, rewrite_sorted_file = True):
 if __name__ == '__main__':
     dir_name = 'files/'
     sorted_file_name = 'new_file.txt'
-    create_sorted_file(dir_name, sorted_file_name)
+    create_sorted_file(dir_name, sorted_file_name, False)
     with open(sorted_file_name, encoding='utf-8') as f:
         for line in f.readlines():
             print(line, end='')
