838dbaf38317bd490b5f0a391a1b3995e93a4a2c
Evgeniy.Gorbunov <Evgeniy.Gorbunov@polaru.com>
2024-03-25 11:22:01 +0300

    final code

--- BookShop/main.py
+++ BookShop/main.py
@@ -1,26 +1,61 @@
+import json
+from prettytable import PrettyTable
 from models import Publisher, Book, Shop, Stock, Sale
 from includes.db import create_db, destroy_db, Session
+
+MODELS = {
+    'publisher': Publisher,
+    'book': Book,
+    'shop': Shop,
+    'stock': Stock,
+    'sale': Sale
+}
+
+
+def insert_data_from_json():
+    with open('assets/tests_data.json', encoding='utf-8') as f:
+        data = json.load(f)
+
+    session = Session()
+    try:
+        for row in data:
+            model = MODELS[row['model']]
+            session.add(model(**row['fields']))
+            session.commit()
+    finally:
+        session.close()
 
 
 def get_sales_for_publisher(publisher):
     session = Session()
-    results =session.query(
-        Book.title, 
-        Shop.name, 
-        Sale.price, 
-        Sale.date_sale) \
-            .join(Publisher).join(Shop).join(Stock).join(Sale) \
-            .filter(Publisher.name.ilike('%' + publisher + '%')).all()
-    
-    # .join(
-    #     Shop).join(Sale).join(Publisher).filter(Publisher.name.ilike('%' + publisher + '%')).all()
-    
-    return results
+
+    try:
+        subq = session.query(Book).join(Publisher).filter(
+            Publisher.name.ilike('%' + publisher + '%')).subquery('publisher_books')
+        results = session.query(
+            subq.c.title.label('book title'),
+            Shop.name.label('shop_name'),
+            Sale.price.label('sale_price'),
+            Sale.date_sale.label('sale_date')
+        ).select_from(Shop).join(Stock).join(Sale).join(subq, Stock.id_book == subq.c.id).all()
+        return results
+    finally:
+        session.close()
 
 
 if __name__ == "__main__":
-    ...
-    q = get_sales_for_publisher('пушкин')
-    print(q)
-    # destroy_db()
-    # create_db()
+    # Функция для заполнения БД тестовыми данными
+    # insert_data_from_json()
+
+    # Функция для вывода данных о продажах книг в магазинах определенного автора
+    publisher = input('Введите название автора: ')
+    q = get_sales_for_publisher(publisher)
+
+    # Вывод данных в таблицу
+    if q:
+        tbl = PrettyTable()
+        tbl.field_names = [
+            'Название книги', 'Название магазина, в котором была куплена эта книга', 'Стоимость покупки', 'Дата покупки']
+        for row in q:
+            tbl.add_row([*row])
+        print(tbl)
--- BookShop/models.py
+++ BookShop/models.py
@@ -4,37 +4,38 @@ from includes.db import Base
 
 
 class Publisher(Base):
-    __tablename__ = 'publishers'
+    __tablename__ = 'publisher'
     name = Column(String(100), unique=True)
 
 
 class Book(Base):
-    __tablename__ = 'books'
+    __tablename__ = 'book'
     title = Column(String(100))
-    id_publisher = Column(Integer, ForeignKey('publishers.id'))
-    
+    id_publisher = Column(Integer, ForeignKey('publisher.id'))
+
     publisher = relationship(Publisher, backref='books')
 
 
 class Shop(Base):
-    __tablename__ = 'shops'
-    name = Column(String(100), unique=True)  
+    __tablename__ = 'shop'
+    name = Column(String(100), unique=True)
 
 
 class Stock(Base):
-    __tablename__ = 'stocks'
-    id_book = Column(Integer, ForeignKey('books.id'))
-    id_shop = Column(Integer, ForeignKey('shops.id'))
+    __tablename__ = 'stock'
+    id_book = Column(Integer, ForeignKey('book.id'))
+    id_shop = Column(Integer, ForeignKey('shop.id'))
     count = Column(Integer)
-    
-    books = relationship(Book, backref='stocks')
-    shops = relationship(Shop, backref='stocks')
+
+    books = relationship(Book, backref='stock')
+    shops = relationship(Shop, backref='stock')
+
 
 class Sale(Base):
-    __tablename__ = 'sales'
-    price = Column(DECIMAL(10,2))
+    __tablename__ = 'sale'
+    price = Column(DECIMAL(10, 2))
     date_sale = Column(DateTime)
-    id_stock = Column(Integer, ForeignKey('stocks.id'))
+    id_stock = Column(Integer, ForeignKey('stock.id'))
     count = Column(Integer)
-    
+
     stocks = relationship(Stock, backref='sales')
