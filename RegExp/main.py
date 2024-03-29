c745fdeaa81ca301f1e4c9df450bbb5c64c49d59
Evgeniy.Gorbunov <Evgeniy.Gorbunov@polaru.com>
2024-03-29 16:27:55 +0300

    final code

--- RegExp/main.py
+++ RegExp/main.py
@@ -2,14 +2,16 @@ from pprint import pprint
 import csv
 import re
 
-def normalize_phone(phone):
-    ...
-    pattern = re.compile(r'(8|\+7)?\s((\d+))\s(\d+)[\s-](\d+)[\s-](\d+)')
+
+def formate_phone(phone):
+    pattern = re.compile(
+        r'(8|\+7)\D?\(?(\d+)\)?\D?(\d+)\D?(\d+)\D?(\d+)\D?\(?[а-я. ]*(\d+)')
     res = pattern.search(phone)
     if res:
-        return '+7' + res.group(2) + res.group(3) + res.group(4) + res.group(5)
-    
-    
+        dop_phone = res.group(5) + res.group(6) if len(res.group(5)
+                                                       ) == 1 else res.group(5) + ' доб.' + res.group(6)
+        return '+7(' + res.group(2) + ')' + res.group(3) + '-' + res.group(4) + '-' + dop_phone
+
 
 # читаем адресную книгу в формате CSV в список contacts_list
 def normalize_phonebook():
@@ -23,12 +25,12 @@ def normalize_phonebook():
         key = '_'.join(fio[:2])
         if key not in dict_phonebook:
             dict_phonebook[key] = {
-                'lastname': fio[0],  
+                'lastname': fio[0],
                 'firstname': fio[1],
                 'surname': fio[2] if len(fio) > 2 else '',
                 'organization': row[3],
                 'position': row[4],
-                'phone': row[5],
+                'phone': formate_phone(row[5]),
                 'email': row[6],
             }
         else:
@@ -39,10 +41,19 @@ def normalize_phonebook():
             if not dict_phonebook[key]['position']:
                 dict_phonebook[key]['position'] = row[4]
             if not dict_phonebook[key]['phone']:
-                dict_phonebook[key]['phone'] = row[5]
+                dict_phonebook[key]['phone'] = formate_phone(row[5])
             if not dict_phonebook[key]['email']:
                 dict_phonebook[key]['email'] = row[6]
-        pprint(dict_phonebook)
+    write_list = [['lastname', 'firstname', 'surname',
+                   'organization', 'position', 'phone', 'email']]
+    for d in dict_phonebook.values():
+        write_list.append(list(d.values()))
+    return write_list
+
 
 if __name__ == "__main__":
-    normalize_phone('+7 (495) 913-04-78')
+    contacts_list = normalize_phonebook()
+    with open("assets/phonebook.csv", "w", encoding="utf-8", newline='') as f:
+        datawriter = csv.writer(f, delimiter=',')
+        # Вместо contacts_list подставьте свой список
+        datawriter.writerows(contacts_list)
