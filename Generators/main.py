67fc53dfb74d500de060688e23fe37ef9558f70c
Evgeniy.Gorbunov <Evgeniy.Gorbunov@polaru.com>
2024-04-14 22:45:02 +0300

    Reload data

--- Generators/main.py
+++ Generators/main.py
@@ -2,16 +2,18 @@ class FlatIterator:
 
     def __init__(self, list_of_list):
         self.list_items = list_of_list
-        
 
     def __iter__(self):
         self.list_len = len(self.list_items)
+        self.list_index = 0
+        self.item_index = -1
         return self
 
     def __next__(self):
-        for i in range(self.list_len):
-            for j in range(len(self.list_items[i])):
-                return self.list_items[i][j]
+        i = self.list_index
+        j = self.item_index + 1
+        
+
 
 def test_1():
 
@@ -28,7 +30,8 @@ def test_1():
 
         assert flat_iterator_item == check_item
 
-    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    assert list(FlatIterator(list_of_lists_1)) == [
+        'a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
 if __name__ == '__main__':
@@ -38,8 +41,6 @@ if __name__ == '__main__':
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
-    
+
     for item in FlatIterator(list_of_lists_1):
         print(item)
-                
-        
