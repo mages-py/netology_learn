3ce51442ac613f6d2ad57fbd3d131dde389773cd
Evgeniy.Gorbunov <Evgeniy.Gorbunov@polaru.com>
2024-06-20 21:05:42 +0300

    reinit

--- dj-homeworks/2.2-databases-2/m2m-relations/articles/models.py
+++ dj-homeworks/2.2-databases-2/m2m-relations/articles/models.py
@@ -6,8 +6,7 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True,
-                              verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
 
     class Meta:
         verbose_name = 'Статья'
@@ -15,21 +14,3 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Категория')
-    articles  = models.ManyToManyField(Article, through='Score')
-
-    class Meta:
-        verbose_name = 'Тег'
-        verbose_name_plural = 'Теги'
-
-    def __str__(self):
-        return self.name
-
-
-class Score(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scores')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scores')
-    is_main = models.BooleanField(default=False)
